5a7fde68aef38e73a18df06abdeb6af8521c3499
dtung8068 <tung.darren@yahoo.com>
2024-12-25 21:24:05 -0500

    Added actions to fix scrolling bug

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import os
 import time
 capa = DesiredCapabilities.CHROME
@@ -11,6 +12,8 @@ capa["pageLoadStrategy"] = "none"
 options = webdriver.ChromeOptions() 
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
+options.add_argument("--start-maximized")
+options.add_argument("--disable-gpu")
 prefs = {'download.default_directory': r"C:\Users\tungd\Downloads\PGA-Tour-Scraper\data",
          'download.prompt_for_download': False}
 options.add_experimental_option('prefs', prefs)
@@ -22,6 +25,7 @@ driver.get('https://www.pgatour.com/stats/detail/02675') #SG Total
 #https://www.pgatour.com/stats/detail/106 Eagles
 #https://www.pgatour.com/stats/detail/02419 Bogeys + Double Bogeys
 wait = WebDriverWait(driver, 20)
+actions = ActionChains(driver)
 
 year_item = driver.find_element(By.CSS_SELECTOR, "[aria-label='Season']")
 wait.until(EC.element_to_be_clickable(year_item))
@@ -34,16 +38,14 @@ for i in year_list:
 wait.until(EC.element_to_be_clickable(year_item))
 year_item.click()
 
-for i in copy_year_list:
+for i in copy_year_list[5:]:
     year_item = driver.find_element(By.CSS_SELECTOR, "[aria-label='Season']")
     wait.until(EC.element_to_be_clickable(year_item))
     year_item.click()
     year = driver.find_element(By.CLASS_NAME, "css-mcc4c4").find_element(By.XPATH, f"//*[contains(text(), '{i}')]")
-    try:
-        wait.until(EC.element_to_be_clickable(year))
-        year.click()
-    except:
-        continue
+    actions.move_to_element(year).perform()
+    wait.until(EC.element_to_be_clickable(year))
+    year.click()
     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[class='chakra-menu__menu-button css-1142au9']")))
     driver.execute_script("window.stop();")
 
@@ -63,18 +65,20 @@ for i in copy_year_list:
         wait.until(EC.element_to_be_clickable(tournament_item))
         tournament_item.click()
         tournament = driver.find_elements(By.CLASS_NAME, "css-mcc4c4")[2].find_element(By.XPATH, f"""//*[contains(text(), "{j}")]""")
-        try:    
-            wait.until(EC.element_to_be_clickable(tournament))
-            tournament.click()
-        except:
-            continue
+        actions.move_to_element(tournament).perform()
+        wait.until(EC.element_to_be_clickable(tournament))
+        tournament.click()
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[aria-label='Download']")))
         driver.execute_script("window.stop();")
         download = driver.find_element(By.CSS_SELECTOR, "[aria-label='Download']")
+        actions.move_to_element(download).perform()
         wait.until(EC.element_to_be_clickable(download))
         download.click()
         description = driver.find_element(By.CSS_SELECTOR, "[class='chakra-text css-d6i95f']").text
-        description = description.replace('Through the ', '')
+        if description.startswith('Through Week Ending'):
+            description = description.replace('Through Week Ending ', ',')
+        else:
+            description = description.replace('Through the ', '')
         while not os.path.exists('data/stats.csv'):
             time.sleep(1)
         os.rename('data/stats.csv', f'data/{i}_{description.split(', ')[1]}_{j}_SG_Total.csv')
