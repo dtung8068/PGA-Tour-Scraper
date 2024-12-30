f917c551a466f5a88499957a27982bf7fdbfac28
dtung8068 <tung.darren@yahoo.com>
2024-12-29 19:26:13 -0500

    Added Money List

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -9,23 +9,36 @@ import time
 capa = DesiredCapabilities.CHROME
 capa["pageLoadStrategy"] = "none"
 
+SAVE_DIRECTORY = 'data/Tournament Results/'
+
 options = webdriver.ChromeOptions() 
 options.add_argument('--ignore-certificate-errors')
 options.add_argument('--ignore-ssl-errors')
 options.add_argument("--start-maximized")
 options.add_argument("--disable-gpu")
-prefs = {'download.default_directory': r"C:\Users\tungd\Downloads\PGA-Tour-Scraper\data",
+prefs = {'download.default_directory': r"C:\Users\tungd\Downloads\PGA-Tour-Scraper\data\Tournament Results",
          'download.prompt_for_download': False}
 options.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(options=options)
-driver.get('https://www.pgatour.com/stats/detail/02674')
+driver.get('https://www.pgatour.com/stats/detail/109')
 #https://www.pgatour.com/stats/detail/02674 SG T2G
 #https://www.pgatour.com/stats/detail/02415 Use to calculate number of pars per round
 #https://www.pgatour.com/stats/detail/107 Birdies
-#https://www.pgatour.com/stats/detail/106 Eagles
-#https://www.pgatour.com/stats/detail/02419 Bogeys + Double Bogeys (Latter inferred from Birdie Bogey Ratio)
+#https://www.pgatour.com/stats/detail/02419 Bogeys
+#https://www.pgatour.com/stats/detail/109 #Money List (Finishing Position)
 wait = WebDriverWait(driver, 20)
 actions = ActionChains(driver)
+
+#Tournament Only
+toggle_item = driver.find_element(By.CSS_SELECTOR, "[aria-label='Time Period']")
+wait.until(EC.element_to_be_clickable(toggle_item))
+toggle_item.click()
+tournament_only = driver.find_elements(By.CLASS_NAME, "css-mcc4c4")[1].find_element(By.XPATH, "//*[contains(text(), 'Tournament Only')]")
+actions.move_to_element(tournament_only).perform()
+wait.until(EC.element_to_be_clickable(tournament_only))
+tournament_only.click()
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[class='chakra-menu__menu-button css-1142au9']")))
+driver.execute_script("window.stop();")
 
 year_item = driver.find_element(By.CSS_SELECTOR, "[aria-label='Season']")
 wait.until(EC.element_to_be_clickable(year_item))
@@ -38,7 +51,7 @@ for i in year_list:
 wait.until(EC.element_to_be_clickable(year_item))
 year_item.click()
 
-for i in copy_year_list:
+for i in copy_year_list[17:]:
     year_item = driver.find_element(By.CSS_SELECTOR, "[aria-label='Season']")
     wait.until(EC.element_to_be_clickable(year_item))
     year_item.click()
@@ -61,7 +74,9 @@ for i in copy_year_list:
     tournament_item.click()
 
     for j in copy_tournament_list:
-        if i == '2022-23' and j in ['Fortinet Championship', 'Sanderson Farms Championship', "Shriners Children's Open", 'The RSM Classic']: #Events held twice
+        if i == '2022-2023' and j in ['Fortinet Championship', 'Sanderson Farms Championship', "Shriners Children's Open", 'The RSM Classic',
+        'ZOZO CHAMPIONSHIP', 'World Wide Technology Championship', 'Butterfield Bermuda Championship', 'Hero World Challenge']: #Events held twice
+            print(f"Duplicated event {j} in {i}")
             continue
         tournament_item = driver.find_element(By.CSS_SELECTOR, "[class='chakra-menu__menu-button css-1142au9']")
         wait.until(EC.element_to_be_clickable(tournament_item))
@@ -86,8 +101,10 @@ for i in copy_year_list:
             description = description.replace('/', '-')
         else:
             description = description.replace('Through the ', '')
-        while not os.path.exists('data/stats.csv'):
+        while not os.path.exists(SAVE_DIRECTORY + 'stats.csv'):
             time.sleep(1)
-        os.rename('data/stats.csv', f'data/{i}_{description.split(', ')[1]}_{j}_SG_T2G.csv')
+        text = j.replace('/', '-')
+        text = text.replace(':', '')
+        os.rename(SAVE_DIRECTORY + 'stats.csv', f'{SAVE_DIRECTORY}/{i}_{description.split(', ')[1]}_{text}_Tournament_Results.csv')
     
 driver.close()
